4813a41b949ff085217dd3edda560ebabac8a0f5
albertodema <albertodema@users.noreply.github.com>
2019-01-16 14:40:48 +0100

    Add files via upload

---
+++ startmaster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# fix Docker dns issue for spark driver
+myip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+sp=' '
+rs=$myip$sp$DNS_LABEL
+cp /etc/hosts ~/hosts.new
+sed -i -e '1 s/^/'"$rs"'\n/' ~/hosts.new
+cp -f ~/hosts.new /etc/hosts
+
+# continue with spark
+chmod +x /master.sh
+/master.sh
